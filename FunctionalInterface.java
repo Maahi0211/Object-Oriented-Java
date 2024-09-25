29d11dd6499e7c87361dc6b63962971342a23b9d
Maahi0211 <maahipatel0211@gmail.com>
2024-09-25 11:45:24 +0530

    functional interface

---
+++ FunctionalInterface.java
@@ -0,0 +1,15 @@
+
+interface A{
+  void show();
+}
+
+public class FunctionalInterface{
+  public static void main(String[] args) {
+    A obj=new A(){
+      public void show(){
+        System.out.println("in show");
+      }
+    };
+    obj.show();
+  }
+}
