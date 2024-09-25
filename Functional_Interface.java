c95668a095f318f4a7986ac0e451f339151bbb34
Maahi0211 <maahipatel0211@gmail.com>
2024-09-25 12:10:56 +0530

    correction

--- Functional_Interface.java
+++ Functional_Interface.java
@@ -1,9 +1,9 @@
-
+@FunctionalInterface
 interface A{
   void show();
 }
 
-public class FunctionalInterface{
+public class Functional_Interface{
   public static void main(String[] args) {
     A obj=new A(){
       public void show(){
